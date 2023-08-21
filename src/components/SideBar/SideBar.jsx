d45081a5c15fcd9fcac4136e5519c051e62d7615
B-Selin <selin@bozkurt.ca>
2023-08-21 09:57:27 -0600

    side bar adjustment

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import logoImage from '../../assets/logo.jpg';
 import './SideBar.css'
@@ -8,22 +8,30 @@ export default function SideBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   function toggleSidebar() {
-    setIsOpen(!isOpen); // Toggle the value
+    setIsOpen(!isOpen);
   }
 
-
   return (
-    <>
-      <button onClick={() => toggleSidebar()}>
-        <img src={logoImage} alt="Logo" className='logo' />
-      </button>
-
-      <Sidebar collapsed={!isOpen}>
-        <Menu>
-          <MenuItem>Home</MenuItem>
-          <MenuItem>About</MenuItem>
-        </Menu>
-      </Sidebar>
-    </>
+    <div className={`l-navbar ${isOpen ? 'show' : ''}`} id="nav-bar">
+      <nav className="nav">
+        <div>
+          <div className="nav_logo">
+            <img src={logoImage} alt="Logo" className="nav_logo-icon" />
+            <span className="logoImage">Celestre</span>
+          </div>
+          <div className="nav_list">
+            <a href="#" className="nav_link active">
+              <i className="bx bx-grid-alt nav_icon"></i>
+              <span className="nav_name">Dashboard</span>
+            </a>
+            {/* Add more nav links here */}
+          </div>
+        </div>
+        <a href="#" className="nav_link">
+          <i className="bx bx-log-out nav_icon"></i>
+          <span className="nav_name">Sign Out</span>
+        </a>
+      </nav>
+    </div>
   );
 }
