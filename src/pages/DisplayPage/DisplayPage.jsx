6babf67c8175c8d4d8ca8b0f21d94601af62d030
B-Selin <selin@bozkurt.ca>
2023-08-22 15:09:33 -0600

    CSS styling for photocards

--- src/pages/DisplayPage/DisplayPage.jsx
+++ src/pages/DisplayPage/DisplayPage.jsx
@@ -12,6 +12,7 @@ export default function DisplayPage() {
         searchResults.map(result => (
           <div key={result.data[0].nasa_id} className="photo-card">
             <img src={result.links[0].href} alt={result.data[0].title} />
+            <div className="overlay"><span>{result.data[0].title}</span></div>
           </div>
         ))
       ) : (
