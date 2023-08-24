3981c2e482613d0ceb8f41ecc2cc05f273c7f288
B-Selin <selin@bozkurt.ca>
2023-08-24 11:30:22 -0600

    required photo model from the CORRECT folder

--- routes/api/photos.js
+++ routes/api/photos.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const photosController = require('../controllers/photos'); 
+const photosController = require('../../controllers/api/photos');
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
+
+
+
 // Define the route for photo upload
-router.post('/upload', upload.single('photo'), photosController.uploadPhoto);
+router.post('/upload', upload.single('photo'),ensureLoggedIn, photosController.uploadPhoto);
+
 
 module.exports = router;
